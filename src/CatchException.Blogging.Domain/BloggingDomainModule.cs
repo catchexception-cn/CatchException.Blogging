d37a9da10375ad79f470a5246d78165e9d11bb0d
Chi Zhang <zliming@outlook.com>
2022-04-09 16:33:07 +0800

    add tagging refrence

--- src/CatchException.Blogging.Domain/BloggingDomainModule.cs
+++ src/CatchException.Blogging.Domain/BloggingDomainModule.cs
@@ -1,10 +1,13 @@
-﻿using Volo.Abp.Domain;
+﻿using CatchException.Tagging;
+
+using Volo.Abp.Domain;
 using Volo.Abp.Modularity;
 
 namespace CatchException.Blogging;
 
 [DependsOn(
     typeof(AbpDddDomainModule),
+    typeof(TaggingDomainModule),
     typeof(BloggingDomainSharedModule)
 )]
 public class BloggingDomainModule : AbpModule
